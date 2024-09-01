b70f277dee548ed9e53c5166f1364188540f8826
martinezbrenda <111657093+martinezbrenda@users.noreply.github.com>
2024-09-01 14:41:31 -0300

    Ejecutor lexico solo

---
+++ ejecutarLexico.bat
@@ -0,0 +1,18 @@
+
+:: Generar el analizador léxico con flex
+flex Lexico.l
+
+gcc lex.yy.c -o compilador.exe
+
+
+:: Pausar para ver cualquier mensaje de error
+pause
+
+:: Ejecutar el compilador con el archivo de prueba
+compilador.exe prueba.txt
+
+:: Limpiar los archivos generados
+@echo off
+del compilador.exe
+del lex.yy.c
+
