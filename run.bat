184d47040ed3c061d44444ab168c391862ce1ccb
maarturello <martinaturello@gmail.com>
2024-09-09 21:27:06 -0300

    comentarios

--- run.bat
+++ run.bat
@@ -2,7 +2,7 @@
 flex Lexico.l
 bison -dyv Sintactico.y
 
-::gcc.exe -o compilador.exe lex.yy.c y.tab.c symbol_table.c Lista.c -lfl
+
 gcc.exe lex.yy.c y.tab.c symbol_table.c Lista.c -o compilador.exe
 
 
